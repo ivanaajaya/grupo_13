118061825255bc6e09ec2c63526ed05dc1861fd7
VauroraS <vsulca@yahoo.com>
2023-09-12 10:04:40 -0300

    model mensaje

--- api/controllers/controller_app.py
+++ api/controllers/controller_app.py
@@ -1,8 +1,7 @@
 from ..model.models_app import App
-from flask import request
-from flask import jsonify
+from flask import request, jsonify
+from ..model.mensajes_model import Mensaje
 from ..database import DatabaseConnection
-
 
 
 # -------------- CLASE CONTROLADOR ----------------
@@ -13,3 +12,55 @@ class App_controller:
 
 # ----------MÉTODOS DE CLASE --------------
     # @classmethod 
+
+class MensajeController:
+
+    @classmethod
+    def get_mensaje(cls, mensaje_id):
+        try:
+            mensaje_instance = Mensaje.mostrar_mensaje(mensaje_id)
+            if mensaje_instance:
+                response_data = {
+                    "id": mensaje_instance.id_mensaje,
+                    "contenido": mensaje_instance.contenido,
+                    "hora_mensaje": mensaje_instance.hora_mensaje,
+                    "fecha_mensaje": mensaje_instance.fecha_mensaje,
+                    "id_usuario": mensaje_instance.id_usuario,
+                    "id_canal": mensaje_instance.id_canal
+                }
+                return jsonify(response_data), 200
+            else:
+                return {"Mensaje": "No se encontró el mensaje"}, 404
+        except Exception as e:
+            print("Error en get_mensaje:", e)
+            return {"Mensaje": "Hubo un error en el servidor"}, 500
+
+    @classmethod
+    def create_mensaje(cls):
+        try:
+            data = request.json
+
+            mensaje = Mensaje(
+                contenido=data.get('contenido', ''),
+                id_usuario=data.get('id_usuario', None),
+                id_canal=data.get('id_canal', None)
+            )
+
+            created = Mensaje.crear_mensaje(mensaje)
+
+            if created:
+                return {'message': 'Mensaje creado con éxito'}, 201
+            else:
+                return {'message': 'No se pudo crear el mensaje'}, 500
+        except Exception as e:
+            print("Error en create_mensaje:", e)
+            return {'message': 'Hubo un error en el servidor'}, 500
+
+    @classmethod
+    def delete_mensaje(cls, mensaje_id):
+        deleted_successfully = Mensaje.eliminar_mensaje(mensaje_id)
+
+        if deleted_successfully:
+            return jsonify({"Mensaje": "Mensaje eliminado correctamente"}), 204
+        else:
+            return {"Mensaje": "No se encontró el mensaje o hubo un problema al eliminarlo"}, 404
--- api/model/mensajes_model.py
+++ api/model/mensajes_model.py
@@ -1,6 +1,6 @@
-from ..database import DatabaseConnection
+from datetime import datetime
 
-class Mensaje: 
+class Mensaje:
     def __init__(self, id_mensaje=None, contenido=None, hora_mensaje=None, fecha_mensaje=None, id_usuario=None, id_canal=None):
         self.id_mensaje = id_mensaje
         self.contenido = contenido
@@ -8,3 +8,56 @@ class Mensaje:
         self.fecha_mensaje = fecha_mensaje
         self.id_usuario = id_usuario
         self.id_canal = id_canal
+
+    @classmethod
+    def crear_mensaje(cls, contenido, id_usuario, id_canal):
+        hora_mensaje = datetime.now().strftime("%H:%M:%S")
+        fecha_mensaje = datetime.now().strftime("%Y-%m-%d")
+
+        query = "INSERT INTO mensajes (contenido, hora_mensaje, fecha_mensaje, id_usuario, id_canal) VALUES (%s, %s, %s, %s, %s);"
+        params = (contenido, hora_mensaje, fecha_mensaje, id_usuario, id_canal)
+
+        result = DatabaseConnection.execute_query(query, params)
+
+        if result:
+            return cls(
+                id_mensaje=result,
+                contenido=contenido,
+                hora_mensaje=hora_mensaje,
+                fecha_mensaje=fecha_mensaje,
+                id_usuario=id_usuario,
+                id_canal=id_canal,
+            )
+        else:
+            return None
+
+    @classmethod
+    def mostrar_mensaje(cls, id_mensaje):
+        query = "SELECT * FROM mensajes WHERE id_mensaje = %s;"
+        params = (id_mensaje,)
+
+        result = DatabaseConnection.fetch_one(query, params)
+
+        if result:
+            return cls(
+                id_mensaje=result[0],
+                contenido=result[1],
+                hora_mensaje=result[2],
+                fecha_mensaje=result[3],
+                id_usuario=result[4],
+                id_canal=result[5],
+            )
+        else:
+            return None
+
+    @classmethod
+    def eliminar_mensaje(cls, id_mensaje):
+        query = "DELETE FROM mensajes WHERE id_mensaje = %s;"
+        params = (id_mensaje,)
+
+        result = DatabaseConnection.execute_query(query, params)
+
+        if result:
+            return True
+        else:
+            return False
