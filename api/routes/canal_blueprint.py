f92c4ecc0359a3f24436d561574a64c55f1c3ac4
user.name like : "Ivana Ajaya <ivana.ajaya01@gmail.com>
2023-09-28 13:52:50 -0300

    todas las modificacions. faltaria ver los mensajes

--- api/routes/canal_blueprint.py
+++ api/routes/canal_blueprint.py
@@ -5,5 +5,5 @@ canal_blueprint = Blueprint('canal_blueprint', __name__)
 
 canal_blueprint.route('/canales/<int:canal_id>', methods=['GET'])(CanalesController.mostrar_canal)
 canal_blueprint.route('/canales/servidor/<int:servidor_id>', methods=['GET'])(CanalesController.mostrar_canales_por_servidor)
-canal_blueprint.route('/canales', methods=['POST'])(CanalesController.crear_canal)
+canal_blueprint.route('/canales', methods=['POST'])(CanalesController.crear_canal)# SI SIRVE, EN EL JSON SE DEBE PASAR EL nombre_canal, id_servidor Y LA RUTA ES http://127.0.0.1:5000/canales 
 canal_blueprint.route('/canales/<int:canal_id>', methods=['DELETE'])(CanalesController.eliminar_canal)
